ad89a2eae0057924ce5bc2ed105ddf2800bbf091
Stav Yaar Bar <anistu@gmail.com>
2025-01-12 15:04:26 +0200

    added open-url page for code param

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const port = 3030
 
-app.use(express.static(path.join(__dirname, 'get-google-code')))
+app.use(express.static(path.join(__dirname, 'open-url-localhost')))
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
